45e5987fe13059c9b78dfbcb7d981eb0c82757aa
benntuecon <zxc123598tw@gmail.com>
2023-05-03 00:24:43 -0700

    config for container

--- pcconfig.py
+++ pcconfig.py
@@ -4,6 +4,7 @@ import pynecone as pc
 config = pc.Config(
     app_name="pynecone_playground",
     # bun_path="$HOME/.bun/bin/bun",
+    api_url="0.0.0.0:8000",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
     port=3000,
